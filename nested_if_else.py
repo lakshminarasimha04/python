6c6ef83cfc73f9fd4254b0cdd7b99f71d9ee60ba
KAREPPAGARI LAKSHMI NARASIMHA <24hr5a3005@mtieat.org>
2026-07-20 21:43:50 +0530

    Add height and age validation for ride access

---
+++ nested_if_else.py
@@ -0,0 +1,10 @@
+height= int(input("enter the height: "))
+if height >=3:
+    print("You can ride")
+    age=int(input("enter the age: "))
+    if age >=18:
+        print("pay 200 rupees")
+    else:
+        print("Pay 500 rupees")
+else:
+    print("Token do not Generate")
